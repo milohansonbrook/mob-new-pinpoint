e7707f01342d9961fb7ed3827a9d3c39854a0f3c
pwhit26 <pwhitney26@klschools.org>
2025-02-02 14:56:12 -0500

    hi gang

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -1,5 +1,4 @@
 package pedroPathing.constants;
-
 import com.pedropathing.localization.Localizers;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.util.CustomFilteredPIDFCoefficients;
