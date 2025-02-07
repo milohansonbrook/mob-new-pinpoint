f097f9373c07e04f19f037a835f8cdc703ab58f5
pwhit26 <pwhitney26@klschools.org>
2025-02-07 13:17:33 -0500

    push it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/RedClip.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/RedClip.java
@@ -1,18 +1,14 @@
 package org.firstinspires.ftc.teamcode.Auton;
 
-import org.firstinspires.ftc.teamcode.AutonPoses.RedClipPoses; //to use
 import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
-import com.pedropathing.pathgen.BezierCurve;
 import com.pedropathing.pathgen.BezierLine;
-import com.pedropathing.pathgen.Path;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Constants;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -23,7 +19,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
-@Autonomous(name = "clippy boy", group = "Autonomous")
+@Autonomous(name = "clippy clip", group = "Autonomous")
 @Config
 public class RedClip extends OpMode {
 
@@ -39,7 +35,7 @@ public class RedClip extends OpMode {
     DcMotor grabMotorR;
 
     //technical poses
-    public static double slurpLowerBound = 0.195;
+    public static double slurpLowerBound = 0.19;
     public static double slurpUpperBound = 0.65;
     public static double armMidPosL = 0.7;
     public static double armMidPosR = (1 - armMidPosL);
@@ -55,11 +51,15 @@ public class RedClip extends OpMode {
 
     String pathState = "init";
     private final Pose startPose = new Pose(0, 0, Math.toRadians(0));
-    private final Pose clipPose = new Pose(23.8, 13.1, Math.toRadians(0));
-    private final Pose prePush1 = new Pose(47.8, -32, Math.toRadians(0));
-    private final Pose helper = new Pose(21.5, -11, Math.toRadians(0));
-    private final Pose set1 = new Pose(46, -22, Math.toRadians(0));
-    private final Pose observe1 = new Pose(7.7, -30, Math.toRadians(0));
+    private final Pose clip1 = new Pose(23.8, 11.1, Math.toRadians(0));
+    private final Pose clip2 = new Pose(23.8, 13.1, Math.toRadians(0));
+    private final Pose clip3 = new Pose(23.8, 15.1, Math.toRadians(0));
+    private final Pose clip4 = new Pose(23.8, 17.1, Math.toRadians(0));
+    private final Pose clip5 = new Pose(23.8, 19.1, Math.toRadians(0));
+    private final Pose prePush1 = new Pose(47.8, -32, Math.toRadians(0)); //front
+    private final Pose helper = new Pose(21.5, -11, Math.toRadians(0)); //like near post
+    private final Pose set1 = new Pose(46, -22, Math.toRadians(0)); //left front of samples
+    private final Pose observe1 = new Pose(7.7, -30, Math.toRadians(0)); //ob zone
     private final Pose set2 = new Pose(47, -30, Math.toRadians(0));
     private final Pose prePush2 = new Pose(46.5, -40.6, Math.toRadians(0));
     private final Pose observe2 = new Pose(10.5, -38.3, Math.toRadians(0));
@@ -70,7 +70,7 @@ public class RedClip extends OpMode {
 
 
     private Follower follower;
-    private PathChain bar1, pushThings, otherBar;
+    private PathChain bar1, push1, push2, push3, bar2, bar3, bar4, bar5;
     Timer opmodeTimer;
     Timer pathTimer;
 
@@ -158,12 +158,12 @@ public class RedClip extends OpMode {
 //        scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePose)));
 //        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
         bar1 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(startPose), new Point(clipPose)))
-                .setLinearHeadingInterpolation(startPose.getHeading(), clipPose.getHeading())
+                .addPath(new BezierLine(new Point(startPose), new Point(clip1)))
+                .setLinearHeadingInterpolation(startPose.getHeading(), clip1.getHeading())
                 .build();
-        pushThings  = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(clipPose), new Point(helper)))
-                .setLinearHeadingInterpolation(clipPose.getHeading(), helper.getHeading())
+        push1 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(clip1), new Point(helper)))
+                .setLinearHeadingInterpolation(clip1.getHeading(), helper.getHeading())
                 .addBezierLine(new Point(helper), new Point(set1))
                 .setLinearHeadingInterpolation(helper.getHeading(), set1.getHeading())
                 .addBezierLine(new Point(set1), new Point(prePush1))
@@ -185,28 +185,55 @@ public class RedClip extends OpMode {
                 .addBezierLine(new Point(observe3), new Point(grabPos))
                 .setLinearHeadingInterpolation(observe3.getHeading(), grabPos.getHeading())
                 .build();
-        otherBar = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(), new Point()))
+        bar2 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(grabPos), new Point(clip1)))
+                .setLinearHeadingInterpolation(grabPos.getHeading(), clip1.getHeading())
+                .addPath(new BezierLine(new Point(clip1), new Point(grabPos)))
+                .setLinearHeadingInterpolation(clip1.getHeading(), grabPos.getHeading())
+                .build();
+        bar3 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(grabPos), new Point(clip1)))
+                .setLinearHeadingInterpolation(grabPos.getHeading(), clip1.getHeading())
+                .addPath(new BezierLine(new Point(clip1), new Point(grabPos)))
+                .setLinearHeadingInterpolation(clip1.getHeading(), grabPos.getHeading())
+                .build();
+        bar4 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(grabPos), new Point(clip1)))
+                .setLinearHeadingInterpolation(grabPos.getHeading(), clip1.getHeading())
+                .addPath(new BezierLine(new Point(clip1), new Point(grabPos)))
+                .setLinearHeadingInterpolation(clip1.getHeading(), grabPos.getHeading())
+                .build();
+        bar5 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(grabPos), new Point(clip1)))
+                .setLinearHeadingInterpolation(grabPos.getHeading(), clip1.getHeading())
                 .build();
     }
     public void setBarPose(double pose){
         lShoulder.setPosition(pose);
-        rShoulder.setPosition(1-pose);
+        rShoulder.setPosition(1 - pose);
+    }
+    public void clip(){
+        grabMotorR.setTargetPosition(200);
+        grabMotorL.setTargetPosition(200);
     }
         public void autonomousPathUpdate() {
             switch (pathState) {
                 case "move to bar":
                     setBarPose(1);
                     turnSlurp.setPosition(0.4);
-                    follower.followPath(bar1, true);
+                    //follower.followPath(bar1, true);
                     wrist.setPosition(0);
+                    clip();
                     setPathState("clip");
                     break;
 
                 case "clip":
-//                    if (!follower.isBusy()){
-//
-//                    }
+                    /*
+                    if (!follower.isBusy()){
+                        clip();
+                   }
+
+                     */
                     break;
 
             }
