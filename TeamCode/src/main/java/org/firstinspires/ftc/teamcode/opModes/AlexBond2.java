9da28f3b7d170fc79009093e19ace12cecdd1e84
abond26 <abond26@klschools.org>
2025-02-25 13:17:10 -0500

    alex

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AlexBond2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AlexBond2.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Auton;
+package org.firstinspires.ftc.teamcode.opModes;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
